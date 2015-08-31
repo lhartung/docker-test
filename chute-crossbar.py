4e18398c6bd10c6fa2bea1becc00616f7adebf46
Nicholas Hyatt <nhyatt@wisc.edu>
2015-08-31 16:26:47 -0500

    Update chute-crossbar.py

--- chute-crossbar.py
+++ chute-crossbar.py
@@ -17,6 +17,49 @@ iptables_options = {
     'action': '--jump {}'
 }
 
+def setupForwardingTable(chain):
+    cmd = [IPTABLES, '--check', 'forward', '--jump', chain]
+    if subprocess.call(cmd) == 0:
+        # Jump to chain already exists.
+        cmd = [IPTABLES, '--flush', chain]
+    else:
+        # Jump rule did not exist, so chain probably does not either.
+        cmd = [IPTABLES, '--new-chain', chain]
+        subprocess.call(cmd)
+
+        cmd = [IPTABLES, '--append', 'forward', '--jump', chain]
+        subprocess.call(cmd)
+
+def getIptablesCommands(chain, ruleDefs):
+    """
+    Generate iptables commands from rule definitions.
+    ruleDefs should be a list of rule definitions.  Each rule definition should
+    be a dictionary of iptables options, which will become one single iptables
+    rule.
+    Example (drop everything between midnight and 5am):
+    ruleDefs = [{
+        'timestart': '00:00',
+        'timestop': '05:00',
+        'action': 'DROP'
+    }]
+    """
+    base_cmd = [IPTABLES, '--append', chain]
+
+    commands = list()
+    for rule in ruleDefs:
+        cmd = list(base_cmd)
+        for key, value in rule.items():
+            if key in iptables_options:
+                option = iptables_options[key].format(value)
+                cmd.extend(option.split())
+        commands.append(cmd)
+
+    return commands
+
+def executeCommands(commands):
+    for cmd in commands:
+        subprocess.call(cmd)
+
 
 class Component(ApplicationSession):
 
@@ -32,49 +75,6 @@ class Component(ApplicationSession):
         commands = getIptablesCommands(CHAIN_NAME, ruleDefs)
         executeCommands(commands)
         return 'Dropping Packets!'
-
-    def setupForwardingTable(chain):
-        cmd = [IPTABLES, '--check', 'forward', '--jump', chain]
-        if subprocess.call(cmd) == 0:
-            # Jump to chain already exists.
-            cmd = [IPTABLES, '--flush', chain]
-        else:
-            # Jump rule did not exist, so chain probably does not either.
-            cmd = [IPTABLES, '--new-chain', chain]
-            subprocess.call(cmd)
-
-            cmd = [IPTABLES, '--append', 'forward', '--jump', chain]
-            subprocess.call(cmd)
-
-    def getIptablesCommands(chain, ruleDefs):
-        """
-        Generate iptables commands from rule definitions.
-        ruleDefs should be a list of rule definitions.  Each rule definition should
-        be a dictionary of iptables options, which will become one single iptables
-        rule.
-        Example (drop everything between midnight and 5am):
-        ruleDefs = [{
-            'timestart': '00:00',
-            'timestop': '05:00',
-            'action': 'DROP'
-        }]
-        """
-        base_cmd = [IPTABLES, '--append', chain]
-
-        commands = list()
-        for rule in ruleDefs:
-            cmd = list(base_cmd)
-            for key, value in rule.items():
-                if key in iptables_options:
-                    option = iptables_options[key].format(value)
-                    cmd.extend(option.split())
-            commands.append(cmd)
-
-        return commands
-
-    def executeCommands(commands):
-        for cmd in commands:
-            subprocess.call(cmd)
 
     @inlineCallbacks
     def onJoin(self, details):
