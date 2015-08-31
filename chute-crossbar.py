c097c5cb329116cae28fb1a9921d1838dd2842e9
Nicholas Hyatt <nhyatt@wisc.edu>
2015-08-31 16:07:43 -0500

    Update chute-crossbar.py

--- chute-crossbar.py
+++ chute-crossbar.py
@@ -3,21 +3,86 @@ from twisted.internet.defer import inlineCallbacks
 
 from autobahn.wamp.types import RegisterOptions, PublishOptions
 from autobahn.twisted.wamp import ApplicationSession, ApplicationRunner
+import subprocess
+
+IPTABLES = "iptables"
+CHAIN_NAME = "PARENTAL_FORWARD"
+
+iptables_options = {
+    'destination': '--destination {}',
+    'mac': '--match mac --mac-source {}',
+    'timestart': '--match time --timestart {}',
+    'timestop': '--match time --timestop {}',
+    'days': '--match time --weekday {}',
+    'action': '--jump {}'
+}
 
 
 class Component(ApplicationSession):
 
     def testFunction(self):
         print 'Test fucntion called!'
-        return 'Holy Cow!'
+        ruleDefs = [{
+            'timestart': '13:00',
+            'timestop': '05:00',
+            'action': 'DROP'
+        }] 
 
-    @inlineCallbacks
-    def onJoin(self, details):
-        print("session attached")
+        setupForwardingTable(CHAIN_NAME)
+        commands = getIptablesCommands(CHAIN_NAME, ruleDefs)
+        executeCommands(commands)
+        return 'Dropping Packets!'
 
-        yield self.register(self.testFunction, 'pd.nick.routerName.parentalControls')
+    def setupForwardingTable(chain):
+        cmd = [IPTABLES, '--check', 'forward', '--jump', chain]
+        if subprocess.call(cmd) == 0:
+            # Jump to chain already exists.
+            cmd = [IPTABLES, '--flush', chain]
+        else:
+            # Jump rule did not exist, so chain probably does not either.
+            cmd = [IPTABLES, '--new-chain', chain]
+            subprocess.call(cmd)
 
-        print("procedure registered")
+            cmd = [IPTABLES, '--append', 'forward', '--jump', chain]
+            subprocess.call(cmd)
+
+    def getIptablesCommands(chain, ruleDefs):
+        """
+        Generate iptables commands from rule definitions.
+        ruleDefs should be a list of rule definitions.  Each rule definition should
+        be a dictionary of iptables options, which will become one single iptables
+        rule.
+        Example (drop everything between midnight and 5am):
+        ruleDefs = [{
+            'timestart': '00:00',
+            'timestop': '05:00',
+            'action': 'DROP'
+        }]
+        """
+        base_cmd = [IPTABLES, '--append', chain]
+
+        commands = list()
+        for rule in ruleDefs:
+            cmd = list(base_cmd)
+            for key, value in rule.items():
+                if key in iptables_options:
+                    option = iptables_options[key].format(value)
+                    cmd.extend(option.split())
+            commands.append(cmd)
+
+        return commands
+
+    def executeCommands(commands):
+        for cmd in commands:
+            subprocess.call(cmd)
+
+        @inlineCallbacks
+        def onJoin(self, details):
+            print("session attached")
+
+            yield self.register(self.testFunction, 'pd.nick.routerName.parentalControls')
+
+            print("procedure registered")
 
 
 if __name__ == '__main__':
@@ -26,5 +91,5 @@ if __name__ == '__main__':
         u"crossbardemo",
         debug_wamp=False,  # optional; log many WAMP details
         debug=False,  # optional; log even more details
-    )   
+    )
     runner.run(Component)
