a4757eab9b32260ca6121961fec9e0fcc0b13956
Lance Hartung <hartung@cs.wisc.edu>
2015-09-01 16:27:02 -0500

    Changed syntax for iptables rule specification.

--- chute-crossbar.py
+++ chute-crossbar.py
@@ -8,14 +8,53 @@ import subprocess
 IPTABLES = "iptables"
 CHAIN_NAME = "PARENTAL_FORWARD"
 
-iptables_options = {
-    'destination': '--destination {}',
-    'mac': '--match mac --mac-source {}',
-    'timestart': '--match time --timestart {}',
-    'timestop': '--match time --timestop {}',
-    'days': '--match time --weekday {}',
-    'action': '--jump {}'
-}
+
+def parse_iptables(options, insideMatch=False):
+    """
+    Parse arguments for iptables provided as a dictionary.
+
+    The key-value pairs are morphed into arguments for iptables.  If the value
+    is a dictionary, then it is assumed to be a (-m/--match) block.
+
+    Example:
+    options = {
+        'd': '192.168.1.1',  # Becomes "-d 192.168.1.1"
+        'time': {
+            'timestart': '00:00',
+            'timestop': '05:00'
+        },  # Becomes "--match time --timestart 20:00 --timestop 04:00"
+        'jump': 'DROP'  # Becomes "--jump DROP"
+    }
+
+    insideMatch: used by recursive call to signify that we have gone down a
+    level and should not recurse.
+    """
+    parts = list()
+    for key, value in options.items():
+        if isinstance(value, dict):
+            if insideMatch:
+                print("Error in iptables rule specification: too many levels")
+                return None
+
+            parts.extend(["--match", key])
+            parts.extend(parse_iptables(value, insideMatch=True))
+
+        elif isinstance(value, list):
+            if len(key) == 1:
+                parts.append("-" + key)
+            else:
+                parts.append("--" + key)
+            parts.extend(value)
+
+        else:
+            if len(key) == 1:
+                parts.append("-" + key)
+            else:
+                parts.append("--" + key)
+            parts.append(value)
+    
+    return parts
+
 
 class Component(ApplicationSession):
 
@@ -63,9 +102,11 @@ class Component(ApplicationSession):
         rule.
         Example (drop everything between midnight and 5am):
         ruleDefs = [{
-            'timestart': '00:00',
-            'timestop': '05:00',
-            'action': 'DROP'
+            'time': {
+                'timestart': '00:00',
+                'timestop': '05:00'
+            },
+            'jump': 'DROP'
         }]
         """
         print("iptables")
@@ -74,10 +115,7 @@ class Component(ApplicationSession):
         commands = list()
         for rule in ruleDefs:
             cmd = list(base_cmd)
-            for key, value in rule.items():
-                if key in iptables_options:
-                    option = iptables_options[key].format(value)
-                    cmd.extend(option.split())
+            cmd.extend(parse_iptables(rule))
             commands.append(cmd)
 
         return commands
